f4b75edd10a54d6ddcadeed5db6c3c785470b86b
TIGeMi <57514820+TIGeMi@users.noreply.github.com>
2021-03-19 22:40:19 +0700

    update plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,11 +7,13 @@ import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
 // import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 // import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
 import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 // import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 // import Image from '@ckeditor/ckeditor5-image/src/image.js';
 // import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js";
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
@@ -19,40 +21,47 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
+import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat.js";
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
-	Alignment,
-	Bold,
-	CodeBlock,
-	Heading,
-	Italic,
-	Link,
-	List,
-	Paragraph,
-	Table,
-	TableToolbar,
-	TextTransformation
+  Bold,
+  CodeBlock,
+  Heading,
+  Italic,
+  Link,
+  List,
+  Paragraph,
+  Table,
+  TableToolbar,
+  TextTransformation,
+  FontSize,
+  Strikethrough,
+  HorizontalLine,
+  RemoveFormat
 ];
 Editor.defaultConfig = {
   toolbar: {
     items: [
-      "codeBlock",
-      "alignment",
-      "heading",
-      "|",
       "bold",
       "italic",
-      "link",
+      "strikethrough",
+      "heading",
+      "|",
+      "codeBlock",
+      "blockQuote",
       "bulletedList",
       "numberedList",
-      "|",
-      "|",
-      "imageUpload",
-      "blockQuote",
       "insertTable",
+      "horizontalLine",
+      "removeFormat",
+      "|",
+      "link",
+      "imageUpload",
+      "|",
       "undo",
       "redo",
     ],
