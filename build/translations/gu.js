b5608eef831a7d1c4dbbb336363cb56aa675c8c5
TIGeMi <57514820+TIGeMi@users.noreply.github.com>
2021-03-19 19:03:02 +0700

    Rebuild

--- build/translations/gu.js
+++ build/translations/gu.js
@@ -1,1 +1,1 @@
-(function(o){const n=o["gu"]=o["gu"]||{};n.dictionary=Object.assign(n.dictionary||{},{"Block quote":" વિચાર ટાંકો",Bold:"ઘાટુ - બોલ્ડ્",Code:"",Italic:"ત્રાંસુ - ઇટલિક્"});n.getPluralForm=function(o){return o!=1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
+!function(n){const i=n.gu=n.gu||{};i.dictionary=Object.assign(i.dictionary||{},{Bold:"ઘાટુ - બોલ્ડ્",Italic:"ત્રાંસુ - ઇટલિક્"}),i.getPluralForm=function(n){return 1!=n}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
